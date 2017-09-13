e1f3a9477d31051bc9136a7f6d517f59294eac16
Emanuele Vannacci <emanuele.vannacci@gmail.com>
2017-09-13 15:37:00 +0200

    fix

--- plots_py/fit_resp_time.py
+++ plots_py/fit_resp_time.py
@@ -6,24 +6,24 @@ alpha = 0.05
 
 job_resp_times_vector = np.genfromtxt("../simulation_results/job_resp_times.csv", delimiter=";")
 plt.axis([0, 25, 0, 0.30])
-res=plt.hist(job_resp_times_vector, bins=70, normed=True, alpha=0.5)
-values = res[1]
-print(values)
+hist_res = plt.hist(job_resp_times_vector, bins=70, normed=True, alpha=0.5)
 
 # exponential lambda
 mean = job_resp_times_vector.mean()
 
-x = np.arange(0, 25, 0.5)
-#param = stats.expon.fit(job_resp_times_vector)
+x = np.arange(0, 100, 0.1)
+# param = stats.expon.fit(job_resp_times_vector)
 # calculate ideal exponential pdf with parameter mean
 pdf_fitted = stats.expon.pdf(x, scale=mean)
 
-#KS TEST
-res = stats.kstest(job_resp_times_vector, lambda x: stats.expon.cdf(x, scale=mean))
-if(res[1] < alpha):
-    print("null hypothesis is rejected")
+# KS TEST
+np.random.seed(12345)
+r = np.random.exponential(scale=mean, size=1000)
+res = stats.ks_2samp(job_resp_times_vector, r)
+if res[1] < alpha:
+    print("null hypothesis is rejected with a p-value=" + str(res[1]) + "\n")
 else:
-    print("null hypothesis cannot be rejected: the samples are drawn by exponential distribution")
+    print("null hypothesis cannot be rejected: the samples are drawn by exponential distribution with p-value="+str(res[1]) + "\n")
 
 plt.plot(x, pdf_fitted, 'r', linewidth=3.0)
 plt.xlabel("Response time values")
