9a840ab92fc835ac68d7ddd7dbe244c3a5b46a50
Laura Trivelloni <laura.trivelloni@gmail.com>
2017-09-16 10:44:00 +0200

    fix min response time varying S with N 20

--- plots_py/n20_find_s_min_resp_time.py
+++ plots_py/n20_find_s_min_resp_time.py
@@ -1,7 +1,11 @@
+import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 import subprocess
 import random
+import math
+
+from matplotlib.ticker import MaxNLocator
 from mpl_toolkits.mplot3d import Axes3D
 
 N = 20
@@ -19,8 +23,13 @@ generate_values(N)
 
 values = np.genfromtxt("../simulation_results/s_min_resp_time.csv", delimiter=";")
 s_vect = values[:, 0]
-resp_vect = values[:, 2]
+s_int = []
+# s_vect, labels = plt.yticks()
+for each in s_vect:
+    s_int.append(int(each))
+plt.xticks(s_int)
 
+resp_vect = values[:, 2]
 
 plt.figure(1)
 plt.xlabel("S")
@@ -28,5 +37,6 @@ plt.ylabel("Response Time")
 plt.title("Minimum Response time achieved in function of S (N=20)")
 plt.grid(True)
 plt.plot(s_vect, resp_vect, 'r', linewidth=3.0)
+
 
 plt.show()
